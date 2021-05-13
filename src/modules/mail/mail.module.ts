beb142aef35339ffc8b947892f2db07a100bd650
Elias Bundala <ebundala@gmail.com>
2021-05-13 11:39:51 +0300

    fix(deployh) use compose as stack fails

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,15 +1,15 @@
 import { SendGridModule } from '@anchan828/nest-sendgrid/dist/sendgrid.module';
 import { Module } from '@nestjs/common';
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 import { PrismaClientModule } from '../prisma-client/prisma-client.module';
 import { MailService } from './mail.service';
-const config = dotenv.config();
+//const config = dotenv.config();
 //console.log(config.parsed.SENDGRID_API_KEY)
 
 @Module({
   imports: [
     PrismaClientModule,
-    SendGridModule.forRoot({ apikey: config.parsed.SENDGRID_API_KEY })],
+    SendGridModule.forRoot({ apikey: process.env.SENDGRID_API_KEY })],
   providers: [MailService],
   exports: [MailService]
 })
